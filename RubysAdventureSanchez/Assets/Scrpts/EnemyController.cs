54e941357dbbabce60045f0171bc8c66f00e02b4
Adam Sanchez <145504826+sghetwet@users.noreply.github.com>
2023-11-19 14:52:27 -0800

    added animation to ruby and enemy
    finished adding the animations to Ruby and the bot, need to fix the bots AI to have it not start moonwalking but that is all that I would need to change.

--- RubysAdventureSanchez/Assets/Scrpts/EnemyController.cs
+++ RubysAdventureSanchez/Assets/Scrpts/EnemyController.cs
@@ -9,14 +9,17 @@ public class EnemyController : MonoBehaviour
     public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2D;
+
     float timer;
     int direction = 1;
 
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -36,23 +39,26 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+            position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + Time.deltaTime * speed;
         }
 
         rigidbody2D.MovePosition(position);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
 
-        void OnCollisionEnter2D(Collision2D other)
+        if (player != null)
         {
-            RubyController player = other.gameObject.GetComponent<RubyController>();
-
-            if (player != null)
-            {
-                player.ChangeHealth(-1);
-            }
+            player.ChangeHealth(-1);
         }
     }
 }
--- RubysAdventureSanchez/Assets/Scrpts/RubyController.cs
+++ RubysAdventureSanchez/Assets/Scrpts/RubyController.cs
@@ -18,11 +18,14 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,11 +34,24 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0)
+            {
                 isInvincible = false;
+            }
         }
     }
     void FixedUpdate()
@@ -51,6 +67,8 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if (isInvincible)
                 return;
 
